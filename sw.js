87941b8efece69d605f9e14c6a5f2cfaf26bdd6c
Breiner Baldovino <breinerb16@hotmail.com>
2026-08-05 15:46:27 -0500

    feat(ui): integra rediseño visual con versión estable

--- sw.js
+++ sw.js
@@ -1,2 +1,42 @@
+const CACHE = "bkc-v1";
 
+const ARCHIVOS = [
+
+    "./",
+
+    "./index.html",
+
+    "./css/style.css",
+
+    "./js/app.js"
+
+];
+
+self.addEventListener("install", event => {
+
+    event.waitUntil(
+
+        caches.open(CACHE)
+
+            .then(cache => cache.addAll(ARCHIVOS))
+
+    );
+
+});
+
+self.addEventListener("fetch", event => {
+
+    event.respondWith(
+
+        caches.match(event.request)
+
+            .then(response =>
+
+                response || fetch(event.request)
+
+            )
+
+    );
+
+});
 
